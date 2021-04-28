9f4186b05f1d43df5d0f4737a4dd5f2d1ec84ed2
FrancisDcruz04 <dcruzfrancis04@gmail.com>
2021-04-28 16:47:32 -0400

    first commit

---
+++ Form2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sample1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ((textBox1.Text == "vishnu" || textBox1.Text == "vikyath" || textBox1.Text == "madhuri")  && textBox2.Text == "admin123")
+            {
+                this.Hide();
+                Form4 f4 = new Form4();
+                f4.ShowDialog();
+            }
+            else
+                MessageBox.Show("Recheck your username and password or not authorized admin");
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 f1 = new Form1();
+            f1.ShowDialog();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
